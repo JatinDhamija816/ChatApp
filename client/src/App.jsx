5c01a996312536626009737e1805e909c8749fd2
JatinDhamija816 <jdhamija816@gmail.com>
2024-04-24 21:35:59 +0530

    Login and Register UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
 
@@ -8,8 +7,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/' element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
     </BrowserRouter>
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
+    const [inputs, setInputs] = useState({ name: '', email: '', password: '', image: '' })
+
+    const HandleChange = (e) => {
+        setInputs(prevState => ({
+            ...prevState, [e.target.name]: e.target.value
+        }))
+    }
+    const HandleRegister = (e) => {
+        e.preventDefault()
+        console.log(inputs)
+    }
+
     return (
-        <div>
-            Register
-        </div>
+        <div className='w-1/3 bg-slate-300 min-h-60 mt-10 drop-shadow-xl rounded-2xl mx-auto' >
+            <div className='pt-5'>
+                <h1 className='text-center text-2xl font-bold'>Register</h1>
+            </div>
+            <div>
+                <form action="">
+                    <div className='w-2/3 mx-auto'>
+                        <div className='m-5'>
+                            <div className='my-2'>
+                                <label className='ml-1 font-semibold '>Name</label>
+                            </div>
+                            <div>
+                                <input type="text" required className='w-full border-none outline-none rounded-lg px-2 p-1' placeholder='Enter Your Name' name='name' onChange={HandleChange} value={inputs.name} />
+                            </div>
+                        </div>
+                        <div className='m-5'>
+                            <div className='my-2'>
+                                <label className='ml-1 font-semibold '>Email</label>
+                            </div>
+                            <div>
+                                <input type="text" required className='w-full border-none outline-none rounded-lg px-2 p-1' placeholder='Enter Your Email' name='email' value={inputs.email} onChange={HandleChange} />
+                            </div>
+                        </div>
+                        <div className='m-5'>
+                            <div className='my-2'>
+                                <label className='ml-1 font-semibold '>Password</label>
+                            </div>
+                            <div>
+                                <input type="password" required className='w-full border-none outline-none rounded-lg px-2 p-1' placeholder='Enter Password' name='password' value={inputs.password} onChange={HandleChange} />
+                            </div>
+                        </div>
+                        <div className='m-5'>
+                            <div className='my-2'>
+                                <label className='ml-1 font-semibold '>Image</label>
+                            </div>
+                            <div>
+                                <input type="file" required className='w-full border-none outline-none rounded-lg p-1' name='image' value={inputs.image} onChange={(e) => setInputs(prevState => ({
+                                    ...prevState, [e.target.name]: e.target.value
+                                }))} />
+                            </div>
+                        </div>
+                        <div className='m-5 justify-center flex'>
+                            <button className='bg-black text-white w-2/3 mb-3 mt-5 rounded-lg text-xl' onClick={HandleRegister}>Register</button>
+                        </div>
+                        <div className='m-5 justify-center flex'>
+                            <button className='mb-10'><Link to='/'>Already Have an Account? Sign in</Link></button>
+                        </div>
+                    </div>
+                </form>
+            </div >
+        </div >
     )
 }
 
